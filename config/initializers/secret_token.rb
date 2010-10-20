c4835e0c10700f3f02aacb60488e044cfd204458
Gk <gk@parish-philp.com>
2010-10-20 14:01:17 -0700

    - Initial commit on BackMyBook v2.0  Woooooo!!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backmybook::Application.config.secret_token = '5e7a54fe940cf2ce0283470adddf52b78bf6e6ee8b2a7aadd5bbf90472d6361adcfa317f9a07bb509c369544f3dac270f0cdd7c4bd55eefb8db689be71ba7242'
