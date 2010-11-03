c78dabe34edcd24318babf1749d0fd7de5f49138
Tay Nguyen <tay.x.nguyen@gmail.com>
2010-11-03 13:12:28 -0700

    Checking in stuff to prep for merge

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,5 +3,9 @@ class CouponsController < ApplicationController
 	def giveaways
 		@author = @current_user.author
 	end
-
+	
+	def freebie_redeem
+		@freebie = Coupon.find_by_giveaway_code( params[:code] )
+		@redeemable = @freebie.redeemable
+	end
 end
