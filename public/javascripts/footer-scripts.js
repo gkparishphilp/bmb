b3cb7b2cf13026f2bc422bb9ae4feeb4560adc4a
William <william@surfshot.com>
2010-11-06 12:36:56 -0700

    Author site frontend work. New style sheets and jquery files.

---
+++ public/javascripts/footer-scripts.js
@@ -0,0 +1,17 @@
+  jQuery(document).ready(function($) {
+	// Accordion
+	$("#accordion").accordion({ header: "h3" });
+
+	// Tabs
+	$('#tabs').tabs();
+	
+	// Slide Menu
+	$('#slide-menu').click(function() {
+	    var $lefty = $('.menu_slider');
+	    $lefty.animate({
+	      left: parseInt($lefty.css('left'),10) == 95 ?
+	        -$lefty.outerWidth() :
+	        95
+	    });
+	  });
+  });
