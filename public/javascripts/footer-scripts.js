801a527f2403a186e4834a16a6eb14a6b146edef
William <william@surfshot.com>
2010-12-02 01:47:50 -0800

    New book page mockup and styles.

--- public/javascripts/footer-scripts.js
+++ public/javascripts/footer-scripts.js
@@ -1,25 +1,24 @@
+function setupLabel() {
+       if ($('.label_radio input').length) {
+        $('.label_radio').each(function(){ 
+            $(this).removeClass('r_on');
+        });
+        $('.label_radio input:checked').each(function(){ 
+            $(this).parent('label').addClass('r_on');
+        });
+    };
+};
   jQuery(document).ready(function($) {
 	// Accordion
 	$( '.datepicker' ).datepicker();
 	$("#accordion").accordion({ header: "h3" });
-	$(".button").button();
-	function setupLabel() {
-	        if ($('.label_radio input').length) {
-	            $('.label_radio').each(function(){ 
-	                $(this).removeClass('r_on');
-	            });
-	            $('.label_radio input:checked').each(function(){ 
-	                $(this).parent('label').addClass('r_on');
-	            });
-	        };
-	    };
-	    $(document).ready(function(){
-	        $('body').addClass('has-js');
-	        $('.label_radio').click(function(){
-	            setupLabel();
-	        });
-	        setupLabel(); 
-	    });
+	$("input.button").button();
+
+    $('body').addClass('has-js');
+    $('.label_radio').click(function(){
+        setupLabel();
+    });
+    setupLabel(); 
 	// Tabs
 	// $('#tabs').tabs();
 	
