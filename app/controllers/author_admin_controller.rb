d3d395166ef0c58090865840826730fc477e2f81
Tay Nguyen <tay.x.nguyen@gmail.com>
2011-01-16 16:47:37 -0800

    - bar graph and pie chart examples

--- app/controllers/author_admin_controller.rb
+++ app/controllers/author_admin_controller.rb
@@ -21,15 +21,29 @@ class AuthorAdminController < ApplicationController
 	
 	def orders
 		@start_date = params[:start_date] || 3.months.ago
-		@end_date = params[:end_date] || Time.now
+		@end_date = params[:end_date] || Time.now 
 		
 		@orders = Order.for_author( @current_author )
-		@orders_past_day = @orders.dated_between( Time.now, 1.day.ago ).for_author( @current_author )
-		@orders_for_period = @orders.dated_between( @start_date, @end_date ).for_author( @current_author )
+		@orders_past_day = @orders.dated_between( Time.now, 1.day.ago )
+		@orders_for_period = @orders.dated_between( @start_date, @end_date )
+		@avg_daily_sales = @orders_for_period.select( "sum(orders.total) as total")
 		
-		@stub_data = [[ ['2010-1-1' , 34], ['2010-2-1', 21], ['2010-3-1', 24], ['2010-4-1', 2344]  ]]
-		@chart_data = [ @orders_for_period.group( "date(orders.created_at)" ).select( "orders.created_at, sum(orders.total) as total" ).map { |o| [o.created_at.to_s, o.total.to_f / 100] } ]
+		@stub_data = [[ ['2010-1-1' , 34], ['2010-2-1', 21], ['2010-3-1', 24], ['2010-4-1', 24], ['2010-5-1', 8]  ]]
+		@daily_sales = [ @orders_for_period.group( "date(orders.created_at)" ).select( "orders.created_at, sum(orders.total) as total" ).map { |o| [o.created_at.to_s, o.total.to_f / 100] } ]
+		@daily_orders = [ @orders_for_period.group( "date(orders.created_at)" ).select( "orders.created_at, count(orders.id) as count" ).map { |o| [o.created_at.to_s, o.count] } ]
+		@orders_by_sku = [ @orders_for_period.group( "orders.sku_id" ).select( "orders.sku_id, count(orders.id) as count").map {|o| [o.sku_id, o.count]} ]
+		@orders_by_sku_xaxis =  @orders_for_period.group( "orders.sku_id" ).select( "orders.sku_id, count(orders.id) as count").map {|o| o.sku_id.to_s} 
+
+		# Tried creating a data array formatted like [[["The Starter",10], ["The Rookie", 13], ...]] and having that print out as the x-axis on a bar chart, but that never worked
+		# So creating a legend instead
+		@legend = Hash.new
 		
+		for a in @orders_by_sku
+			for b in a
+				@legend[b[0]] = Sku.find( b[0].to_i).title
+			end
+		end
+				
 		render :layout  => 'reports'
 	end
 	
@@ -50,3 +64,4 @@ class AuthorAdminController < ApplicationController
 
 end
 
+# o
