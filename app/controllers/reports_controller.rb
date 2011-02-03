7b91ccbb6e901c8749c469185057c358f9e7aeae
Tay Nguyen <tay.x.nguyen@gmail.com>
2011-02-02 22:34:08 -0800

    Reporting tweaks

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,22 +1,19 @@
 class ReportsController < ApplicationController
 
 	def sales
-		@start_date = params[:start_date] || 3.months.ago
+		@start_date = params[:start_date] || 1.months.ago
 		@end_date = params[:end_date] || Time.now 
 		
 		@orders = Order.for_author( @current_author )
 		@orders_past_day = @orders.dated_between( Time.now, 1.day.ago )
 		@orders_for_period = @orders.dated_between( @start_date, @end_date )
-		@total_sales = @orders_for_period.select( "sum(orders.total) as total").all
-		#@avg_daily_sales = @orders_for_period.select( "sum(orders.total) as total") / (@end_date - @start_date)
+		@total_sales = @orders_for_period.select( "sum(orders.total) as total").first.total / 100
+		@avg_daily_sales = @orders_for_period.select( "sum(orders.total) as total").first.total / (@end_date.to_date - @start_date.to_date) / 100
 
-		
-		@stub_data = [[ ['2010-1-1' , 34], ['2010-2-1', 21], ['2010-3-1', 24], ['2010-4-1', 24], ['2010-5-1', 8]  ]]
 		@daily_sales = [ @orders_for_period.group( "date(orders.created_at)" ).select( "orders.created_at, sum(orders.total) as total" ).map { |o| [o.created_at.to_s, o.total.to_f / 100] } ]
 		@daily_orders = [ @orders_for_period.group( "date(orders.created_at)" ).select( "orders.created_at, count(orders.id) as count" ).map { |o| [o.created_at.to_s, o.count] } ]
 		@orders_by_sku = [ @orders_for_period.group( "orders.sku_id" ).select( "orders.sku_id, count(orders.id) as count").map {|o| [o.sku_id, o.count]} ]
 		@orders_by_sku_xaxis =  @orders_for_period.group( "orders.sku_id" ).select( "orders.sku_id, count(orders.id) as count").map {|o| o.sku_id.to_s} 
-
 
 		# Creating a data array with the title in it, like [[["The Starter",10], ["The Rookie", 13], ...]] for pie chart
 		for a in @orders_by_sku
@@ -24,7 +21,7 @@ class ReportsController < ApplicationController
 				b[0] = '(' + b[1].to_s + ')' + ' ' + Sku.find( b[0].to_i).title[0,30]
 			end
 		end
-				
+	
 		render :layout  => 'reports'
 	
 	end
