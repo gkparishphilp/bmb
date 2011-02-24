7c894c325fccff7d95294824e8b48c97338b60a3
tay <tay.x.nguyen@gmail.com>
2011-02-23 16:54:45 -0800

    Display tweak

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -5,7 +5,7 @@ class MarketingMailer < ActionMailer::Base
 		@author = author
 		@message = message
 		
-		mail :to => "#{@author.user.email}", :from => "#{@author.contact_email}", :subject => "#{message.subject} (Test Message)"
+		mail :to => "#{@author.user.email}", :from => "#{@author.pen_name} <#{@author.contact_email}>", :subject => "#{message.subject} (Test Message)"
 	end
 
 	def send_to_subscriber( message, author, subscription, delivery_record )
@@ -14,7 +14,7 @@ class MarketingMailer < ActionMailer::Base
 		@message = message
 		@subscription = subscription
 		@delivery_record = delivery_record
-		mail :to => "#{@subscription.subscriber.email}", :from => "#{@author.contact_email}", :subject => "#{message.subject}"
+		mail :to => "#{@subscription.subscriber.email}", :from => "#{@author.pen_name} <#{@author.contact_email}>", :subject => "#{message.subject}"
 	end
 	
 end
