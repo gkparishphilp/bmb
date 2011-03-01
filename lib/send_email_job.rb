d9758a7df11445633a1b1d05ce5de1d5acaa7ac5
Tay Nguyen <tay.x.nguyen@gmail.com>
2011-02-28 20:43:02 -0800

    Commenting out SES calls for testing

--- lib/send_email_job.rb
+++ lib/send_email_job.rb
@@ -1,8 +1,10 @@
 class SendEmailJob  < Struct.new(:user, :source, :subject, :html_body, :email)
 	def perform
 		if user.valid?
-			ses = AWS::SES::Base.new(:access_key_id => AWS_ID, :secret_access_key => AWS_SECRET)
-			ses.send_email( :to => user.email, :source => source, :subject => subject, :html_body => html_body)
+			#ses = AWS::SES::Base.new(:access_key_id => AWS_ID, :secret_access_key => AWS_SECRET)
+			#ses.send_email( :to => user.email, :source => source, :subject => subject, :html_body => html_body)
+			user.status = 'emailed'
+			user.save
 		end
 	end
 
