9dc778c563f2b1bc6245ccc436fb28e7a050269f
tay <tay.x.nguyen@gmail.com>
2011-04-08 12:41:50 -0700

    Changing # of sales shown back to how it was originally

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,7 +8,7 @@ class ReportsController < ApplicationController
 		
 		@orders = Order.for_author( @current_author )
 		# Active relation just passes in the day, starting at midnight.  So forwarding the end date by 1 day to capture today's purchases
-		@orders_past_day = @orders.dated_between( 1.day.ago.getutc, Time.now.getutc + 1.day ).successful.order('created_at desc').limit(10)
+		@orders_past_day = @orders.dated_between( 1.day.ago.getutc, Time.now.getutc + 1.day ).successful.order('created_at desc')
 		@orders_for_period = @orders.dated_between( @start_date.to_date.beginning_of_day.getutc, @end_date.to_date.end_of_day.getutc).successful
 		@orders_for_week_ending = @orders.dated_between( (@week_ending - 7.days).getutc, @week_ending.getutc).successful
 		@total_sales = @orders_for_period.select( "sum(orders.total) as total").first.total / 100
