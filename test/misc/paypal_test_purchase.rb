8c392b1c784f189786015625295154f4863ffcfd
Tay Nguyen <tay.x.nguyen@gmail.com>
2011-06-28 10:34:48 -0700

    Oops, broke the search functionality in orders/admin.  Fixing now.

--- test/misc/paypal_test_purchase.rb
+++ test/misc/paypal_test_purchase.rb
@@ -94,3 +94,9 @@ else
 	puts "Transfer failed!"
 end
 puts "Transfer response is #{transfer_response.inspect}"	
+
+=begin
+Here's the return from a paypal express purchase
+Get it by doing a transaction and then querying for the details in rails console: data= EXPRESS_GATEWAY.details_for('EC-5597694722013783L')
+#<ActiveMerchant::Billing::PaypalExpressResponse:0x000001053c2d90 @params={"timestamp"=>"2011-06-28T17:30:51Z", "ack"=>"Success", "correlation_id"=>"9d6d75ab3a098", "version"=>"59.0", "build"=>"1936884", "token"=>"EC-5597694722013783L", "payer"=>"buyer_1269634012_per@sky360corp.com", "payer_id"=>"7T28RWEPAA9CW", "payer_status"=>"verified", "salutation"=>nil, "first_name"=>"Test", "middle_name"=>nil, "last_name"=>"User", "suffix"=>nil, "payer_country"=>"US", "payer_business"=>nil, "name"=>"Test User", "street1"=>"1 Main St", "street2"=>nil, "city_name"=>"San Jose", "state_or_province"=>"CA", "country"=>"US", "country_name"=>"United States", "phone"=>nil, "postal_code"=>"95131", "address_owner"=>"PayPal", "address_status"=>"Confirmed", "order_total"=>"2.99", "order_total_currency_id"=>"USD", "shipping_total"=>"0.00", "shipping_total_currency_id"=>"USD", "handling_total"=>"0.00", "handling_total_currency_id"=>"USD", "tax_total"=>"0.00", "tax_total_currency_id"=>"USD", "address_id"=>nil, "external_address_id"=>nil, "insurance_total"=>"0.00", "insurance_total_currency_id"=>"USD", "shipping_discount"=>"0.00", "shipping_discount_currency_id"=>"USD"}, @message="Success", @success=true, @test=true, @authorization=nil, @fraud_review=false, @avs_result={"code"=>nil, "message"=>nil, "street_match"=>nil, "postal_match"=>nil}, @cvv_result={"code"=>nil, "message"=>nil}> 
+=end
