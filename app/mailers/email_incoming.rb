551b7e3df4355491b7fdbe53b9e0f52a5eda9a5f
Tay Nguyen <tay.x.nguyen@gmail.com>
2011-07-31 17:53:17 -0700

    -Why doesn't the ActionMailer receive in email_incoming.rb process the unsubscribe???

--- app/mailers/email_incoming.rb
+++ app/mailers/email_incoming.rb
@@ -4,25 +4,22 @@ class EmailIncoming < ActionMailer::Base
 		logger.info("Got a mail from #{mail.from} about: #{mail.subject}")		
 		mail_from = mail.from.to_s
 		mail_body = mail.body.to_s
-		logger.info("MAIL_FROM: #{mail_from}" )
-		logger.info("MAIL_BODY: #{mail_body}" )
-		logger.info("END MAIL_BODY")
 		# Process abuse complaints by unsubscribing the user	
 		if mail_from.match(/email-abuse.amazonses.com/i)
-			logger.info("Inside the abuse loop")
 			#Strip all '=' and carriage returns in the body because Amazon inserts them as line breaks
 			body=''
 			mail_body.each_line { |line|
-				logger.info("LINE: #{line}")
-				logger.info("BODY: #{body}")
 				line.chomp!
 				line.chomp!('=')
 				body += line
 			}
 			code_array = body.match(/http:\/\/backmybook.com\/unsubscribe\/(.{40})">/)
-			code = code_array[1]
-			logger.info("BODY: #{mail_body}")
+			code = code_array[1].to_s
 			logger.info("CODE = #{code}")
+			
+			if sub = EmailSubscribing.find_by_unsubscribe_code( code )
+				sub.update_attributes :status => "unsubscribed - spam abuse"
+			end
 		end
 	end
 end
